afe4fb5e8b99ce1fca3b130ee968cecb07cb2e9d
Evan Shizas <shizasevan@gmail.com>
2022-06-17 23:08:39 -0400

    Finished Part 1 & Nearly 2...
    Part 2, "XMLCreate.java" needs to have the entire XML printed to the terminal. I am not too sure on how to do that. Can someone else give it a try?
    
    - Evan

--- a18-accessing-external-data/src/XMLCreate.java
+++ a18-accessing-external-data/src/XMLCreate.java
@@ -1,40 +1,49 @@
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
+/**
+ * Creates and prints an XML file.
+ * 
+ * modified     20220617
+ * date         20220615
+ * @filename    XMLCreate.java
+ * @author      Evan Shizas
+ * @author      Alvin Chan
+ * @author      Hammad Hassan
+ * @author		Oscar Lam
+ * @version     1.0.0
+ * @see         A18 - Accessing External Data
+ */
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import java.io.*;
+import javax.xml.parsers.ParserConfigurationException;
 
-public class XMLCreate extends JFrame {
+public class XMLCreate {
+	public static void main(String[] args) throws ParserConfigurationException, TransformerException {
+		try {
+			OutputStream fout = new FileOutputStream("course-information.xml");
+			OutputStream bout = new BufferedOutputStream(fout);
+			OutputStreamWriter out = new OutputStreamWriter(bout, "8859_1");
 
-	private JPanel contentPane;
+			out.write("<?xml version=\"1.0\" ");
+			out.write("encoding=\"ISO-8859-1\"?>\r\n");
+			out.write("<class>\r\n");
+			out.write("<code>ISC4U1-01</code>\r\n");
+			out.write("<description>Computer Science, Grade 12, University</description>\r\n");
+			out.write("<schoolBoard>TDSB</schoolBoard>\r\n");
+			out.write("<teacher>teacherbot</teacher>\r\n");
+			out.write("<fileType>Unmodified</fileType>\r\n");
+			out.write("</class>\r\n");
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					XMLCreate frame = new XMLCreate();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+			out.flush();
+			out.close();
+			
+			// Print out xml file in terminal...
+		}
+
+		catch (UnsupportedEncodingException e) {
+			System.out.println("This VM does not support the Latin-1 character set.");
+		}
+
+		catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
 	}
-
-	/**
-	 * Create the frame.
-	 */
-	public XMLCreate() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
-	}
-
 }
